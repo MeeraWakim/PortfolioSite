3a83c5624e2a9ef89dea3c883bb297d8d171417e
MeeraWakim <meera.wakim925@gmail.com>
2018-09-12 11:27:39 -0500

    vue and vuetify installation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
     <v-app>
+
         <v-toolbar flat>
             <v-spacer></v-spacer>
             <v-btn flat>ABOUT ME</v-btn>
@@ -14,20 +15,39 @@
                     <v-flex>
                     <v-layout row wrap>
                         <h3 class="display-3">Hi, I'm Meera.</h3>
-
-    </v-layout>
+                </v-layout>
                         <v-divider class="my-3"></v-divider>
                     </v-flex>
                 </v-layout>
             </v-container>
         </v-jumbotron>
+
+        <v-flex>
+            <v-btn ref="button" color="primary" block @click="$vuetify.goTo('#element-near-top', { offset: -500 })">scroll</v-btn>
+        </v-flex>
     </v-app>
 </template>
 
 <script>
+import AboutMe from './components/AboutMe.vue'
+import * as easings from 'vuetify/es5/util/easing-patterns'
+export default {
+
+name: 'App',
+components: {
+    AboutMe,
+},
+data () {
+      return {
+
+      }
+},
+};
 
 </script>
 
 <style>
-
+#element-near-top {
+  height: 100px;
+}
 </style>
